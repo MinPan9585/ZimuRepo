17a8ebe1f767afc521bad4e653933ad660ded9c2
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-10-14 12:08:44 +0800

    1014 add bullet

--- ZimuTestProject0810/Assets/Scripts/Player.cs
+++ ZimuTestProject0810/Assets/Scripts/Player.cs
@@ -7,10 +7,24 @@ public class Player : MonoBehaviour
 {
     public int coinNum = 0;
     public Text coinText;
+    public GameObject bullet;
+    public float bulletForce;
 
     private void Update()
     {
-        coinText.text = "Coins: " + coinNum;    
+        //coinText.text = "Coins: " + coinNum;
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletIns = Instantiate(bullet, transform.position + new Vector3(0.15f,0.7f,0), transform.rotation);
+            Rigidbody rb = bulletIns.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(transform.GetChild(1).forward * bulletForce, ForceMode.Impulse);
+            }
+        }
     }
+
 
 }
